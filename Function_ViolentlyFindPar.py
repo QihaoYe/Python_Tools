62ea9706e1d297a0434ef6188e2eec4541599050
QihaoYe <11610812@mail.sustc.edu.cn>
2017-08-22 17:10:06 +0800

    Update Function_ViolentlyFindPar.py

--- Function_ViolentlyFindPar.py
+++ Function_ViolentlyFindPar.py
@@ -8,7 +8,9 @@ def FindBestPar(test):
     allproducts = list(product(*test))
     return allproducts
 
+'''
 #A simple example of this function
 a = FindBestPar([[1,2],[3,4],[5,6],[7,8],[9,10]])
 for b in a:
     print(b)
+'''
