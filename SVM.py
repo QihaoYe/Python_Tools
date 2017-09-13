5c418f016368dd7c244d3ce738a3d8a45cf5e4b8
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-13 20:19:38 +0800

    naive update

--- SVM.py
+++ SVM.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
-# coding: utf-8
+# -*- coding: utf-8 -*-
 __author__ = 'Yee_172'
 __data__ = '2017/9/12'
+
+import sys
+import numpy as np
+
+
--- polygon_area.py
+++ polygon_area.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# coding: utf-8
+# -*- coding: utf-8 -*-
 __author__ = 'Yee_172'
 __data__ = '2017/8/15'
 
