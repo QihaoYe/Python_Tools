9517c2e12425962dccafec439d3d9e4280697e1b
QihaoYe <11610812@mail.sustc.edu.cn>
2017-10-09 22:11:18 +0800

    update

---
+++ EulersFunction.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# coding: utf-8
+__author__ = 'Yee_172'
+__data__ = '2017/10/09'
+
+
+def generate_division(n):
+    d = 2
+    while d * d <= n:
+        flag = 1
+        while n % d is 0:
+            if flag:
+                yield d
+            flag = 0
+            n //= d
+        d += 1
+    if n > 1:
+        yield n
+
+
+def phi(n):
+    assert n > 0
+    
+    for d in generate_division(n):
+        n //= d
+        n *= d - 1
+    return n
+
+# ---[test zone]---
+print(phi(20))
